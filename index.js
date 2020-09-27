929dd3d18fbdb3d78be51d0a81f39e33398101fa
ysaiascampos <ysaiascampos@gmail.com>
2020-09-27 10:54:24 -0300

    continuacion

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const app = express();
 // Puerto de la app
 const PORT  = process.env.PORT || 4000;
 
-// Definir la pagina principal
-app.get('/', (req, res) => {
-    res.send('hola mundo');
-});
-
 //arrancar la app
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
