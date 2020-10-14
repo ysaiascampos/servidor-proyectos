bf85f9dbd943dacf4615d801d1905b5321e8fc29
ysaiascampos <ysaiascampos@gmail.com>
2020-10-13 22:03:53 -0300

    ajustes para deployment

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // Puerto de la app
-const port  = process.env.port || 4000;
+const PORT  = process.env.PORT || 4000;
 
 //importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 //arrancar la app
-app.listen(port,'0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
